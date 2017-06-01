646740b4986033fadeded3c8b5e7c50f5948749d
musyoku <musyoku@users.noreply.github.com>
2017-06-01 21:52:30 +0900

    :v:

--- rnn/train.py
+++ rnn/train.py
@@ -18,6 +18,8 @@ def get_current_learning_rate(opt):
 		return opt.lr
 	if isinstance(opt, optimizers.Adam):
 		return opt.alpha
+	if isinstance(opt, optimizers.SGD):
+		return opt.lr
 	raise NotImplementationError()
 
 def get_optimizer(name, lr, momentum):
@@ -25,10 +27,17 @@ def get_optimizer(name, lr, momentum):
 		return optimizers.NesterovAG(lr=lr, momentum=momentum)
 	if name == "adam":
 		return optimizers.Adam(alpha=lr, beta1=momentum)
+	if name == "sgd":
+		return optimizers.SGD(lr=lr)
 	raise NotImplementationError()
 
 def decay_learning_rate(opt, factor, final_value):
 	if isinstance(opt, optimizers.NesterovAG):
+		if opt.lr <= final_value:
+			return
+		opt.lr *= factor
+		return
+	if isinstance(opt, optimizers.SGD):
 		if opt.lr <= final_value:
 			return
 		opt.lr *= factor
